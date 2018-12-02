dc8f96bb1d422d8d1af1c7e78f254596ae27337d
ssartell <ssartell@rightpoint.com>
2018-12-02 12:35:40 -0600

    update

--- day02/part2.js
+++ day02/part2.js
@@ -3,7 +3,7 @@ var levenshtein = require('fast-levenshtein');
 var C = require('js-combinatorics');
 
 var parseInput = R.pipe(R.trim, R.split('\r\n'));
-var levOf2 = arr => C.bigCombination(arr, 2).find(pair => R.apply(levenshtein.get, pair) === 1);
+var levOf2 = arr => C.bigCombination(arr, 2).find(R.pipe(R.apply(levenshtein.get), R.equals(1)));
 var commonLetters = R.pipe(R.apply(R.zip), R.filter(R.apply(R.equals)), R.map(R.head), R.join(''));
 
 var solution = R.pipe(parseInput, levOf2, commonLetters);
