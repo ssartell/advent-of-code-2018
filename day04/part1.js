e0290a512ae0e0ee1470bd3517dc527bc6874938
ssartell <ssartell@rightpoint.com>
2018-12-04 17:12:16 -0600

    update

--- day04/part1.js
+++ day04/part1.js
@@ -7,10 +7,10 @@ var parseInput = R.pipe(R.trim, R.split('\n'), R.map(parseLine));
 var fillIds = R.pipe(R.mapAccum((id, x) => [x.id || id, R.assoc('id', x.id || id, x)], 0), R.last);
 var withoutShifts = R.filter(x => x.action !== 'begins shift');
 var expandPair = (sleep, wake) => R.map(x => ({ min: x, id: sleep.id }), R.range(sleep.min, wake.min));
-var expandMins = R.pipe(R.splitEvery(2), R.map(R.apply(expandPair)), R.flatten);
+var expandMins = R.pipe(R.sortBy(x => x.datetime), fillIds, withoutShifts, R.splitEvery(2), R.map(R.apply(expandPair)), R.flatten);
 var group = f => R.pipe(R.sortBy(f), R.groupWith((a, b) => f(a) === f(b)));
 var biggestArray = R.reduce((acc, x) => acc.length > x.length ? acc : x, []);
 var answer = record => record.min * record.id;
-var solution = R.pipe(parseInput, R.sortBy(x => x.datetime), fillIds, withoutShifts, expandMins, group(x => x.id), biggestArray, group(x => x.min), biggestArray, R.head, answer);
+var solution = R.pipe(parseInput, expandMins, group(x => x.id), biggestArray, group(x => x.min), biggestArray, R.head, answer);
 
 module.exports = solution;
