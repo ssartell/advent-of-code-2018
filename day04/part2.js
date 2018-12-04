dacb752bac0d6f605e440a7fe728e9d303390fe6
ssartell <ssartell@rightpoint.com>
2018-12-04 17:03:28 -0600

    update

--- day04/part2.js
+++ day04/part2.js
@@ -1,17 +1,17 @@
 var R = require('ramda');
 
 var lineRegex = /\[(\d+-\d+-\d+ \d+:(\d+))\](?: Guard #(\d+))? ((?:falls asleep)|(?:wakes up)|(?:begins shift))/;
-var parseLine = R.pipe(R.match(lineRegex), R.tail, R.zipObj(['datetime', 'min', 'id', 'action']))
+var parseLine = R.pipe(R.match(lineRegex), R.tail, R.zipObj(['datetime', 'min', 'id', 'action']), R.evolve({min: parseInt, id: parseInt}));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(parseLine));
 
 var id;
-var fixIds = R.forEach(x => { x.id = id = x.id || id; });
-var filterBeginShifts = R.filter(x => x.action !== 'begins shift');
-var expandPair = pair => R.map(x => ({ min: x, id: pair[0].id }), R.range(parseInt(pair[0].min), parseInt(pair[1].min)));
-var expandMinutes = R.pipe(R.splitEvery(2), R.map(expandPair), R.flatten);
-var groupProp = prop => R.pipe(R.sortBy(R.prop(prop)), R.groupWith((a, b) => R.prop(prop, a) === R.prop(prop, b)));
-var maxArrayLength = R.reduce((acc, x) => acc.length > x.length ? acc : x, []);
-var answer = record => record.min * parseInt(record.id);
-var solution = R.pipe(parseInput, R.sortBy(R.prop('datetime')), fixIds, filterBeginShifts, expandMinutes, groupProp('id'), R.map(groupProp('min')), R.unnest, maxArrayLength, R.head, answer);
+var fillIds = R.forEach(x => { x.id = id = x.id || id; });
+var withoutShifts = R.filter(x => x.action !== 'begins shift');
+var expandPair = (sleep, wake) => R.map(x => ({ min: x, id: sleep.id }), R.range(sleep.min, wake.min));
+var expandMins = R.pipe(R.splitEvery(2), R.map(R.apply(expandPair)), R.flatten);
+var group = f => R.pipe(R.sortBy(f), R.groupWith((a, b) => f(a) === f(b)));
+var biggestArray = R.reduce((acc, x) => acc.length > x.length ? acc : x, []);
+var answer = x => x.min * x.id;
+var solution = R.pipe(parseInput, R.sortBy(x => x.datetime), fillIds, withoutShifts, expandMins, group(x => x.id), R.map(group(x => x.min)), R.unnest, biggestArray, R.head, answer);
 
 module.exports = solution;
