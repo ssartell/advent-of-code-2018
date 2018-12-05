23d361768a55123270bb5382665281fe855a9867
ssartell <ssartell@rightpoint.com>
2018-12-05 09:52:08 -0600

    update

--- day05/part2.js
+++ day05/part2.js
@@ -1,6 +1,5 @@
 var R = require('ramda');
 var Stack = require('mnemonist/stack');
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 var parseInput = R.pipe(R.trim, R.split(''));
 
@@ -10,7 +9,7 @@ var dontReact = (stack, unit) => { stack.push(unit); return stack; };
 var isSkipped = R.curry((skip, stack, unit) => skip === unit.toLowerCase());
 var attemptReaction = skip => R.ifElse(isSkipped(skip), R.identity, R.ifElse(shouldReact, react, dontReact));
 var reactPolymer = skip => R.reduce(attemptReaction(skip), new Stack());
-var shortestPolymer = polymer => R.reduce((min, letter) => R.min(min, reactPolymer(letter)(polymer).size), Infinity, alphabet);
+var shortestPolymer = polymer => R.reduce((min, letter) => R.min(min, reactPolymer(letter)(polymer).size), Infinity, 'abcdefghijklmnopqrstuvwxyz');
 
 var solution = R.pipe(parseInput, shortestPolymer);
 
