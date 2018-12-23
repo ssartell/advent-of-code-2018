68f6bc3ec5e89459ff65033b21ad042f70f37694
ssartell <ssartell@rightpoint.com>
2018-12-23 13:01:34 -0600

    update

--- day22/part2.js
+++ day22/part2.js
@@ -15,19 +15,19 @@ var getGeoIndex = (depth, target, coords) => {
     return getErosionLevel(depth, target, {x: coords.x - 1, y: coords.y}) * getErosionLevel(depth, target, {x: coords.x, y: coords.y - 1});
 };
 var getErosionLevel = R.memoizeWith(getKey, (depth, target, coords) => (getGeoIndex(depth, target, coords) + depth) % 20183);
-var getType = R.memoizeWith(getKey, (depth, target, coords) => getErosionLevel(depth, target, coords) % 3);
+var getTerrain = R.memoizeWith(getKey, (depth, target, coords) => getErosionLevel(depth, target, coords) % 3);
 
 // movement
 var dirs = [{x: 0, y: -1}, {x: -1, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}];
 var step = (pos, dir) => ({x: pos.x + dir.x, y: pos.y + dir.y});
-var isInBounds = pos => 0 <= pos.x && 0 <= pos.y
+var isInBounds = pos => 0 <= pos.x && 0 <= pos.y;
 var manhattan = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 
 // tools
 var tools = { neither: 0, torch: 1, gear: 2 };
-var isValidTools = (tool, type) => (type === terrains.rocky && (tool === tools.torch || tool === tools.gear)) 
-                                || (type === terrains.wet && (tool === tools.neither || tool === tools.gear))
-                                || (type === terrains.narrow && (tool === tools.neither || tool === tools.torch));
+var isValidTools = (tool, terrain) => (terrain === terrains.rocky && (tool === tools.torch || tool === tools.gear)) 
+                                || (terrain === terrains.wet && (tool === tools.neither || tool === tools.gear))
+                                || (terrain === terrains.narrow && (tool === tools.neither || tool === tools.torch));
 
 var run = input => {
     var depth = input.depth;
@@ -39,16 +39,16 @@ var run = input => {
         for(var dir of dirs) {
             var nextPos = step(i.pos, dir);
             if (!isInBounds(nextPos)) continue;
-            var nextType = getType(depth, target, nextPos);
-            if (!isValidTools(i.tool, nextType)) continue;
-            yield {pos: nextPos, t: i.t + 1, tool: i.tool, type: nextType, last: i};
+            var nextTerrain = getTerrain(depth, target, nextPos);
+            if (!isValidTools(i.tool, nextTerrain)) continue;
+            yield {pos: nextPos, t: i.t + 1, tool: i.tool, terrain: nextTerrain, last: i};
         }
 
-        var type = getType(depth, target, i.pos);
+        var terrain = getTerrain(depth, target, i.pos);
         for(var tool of R.values(tools)) {
             if (tool === i.tool) continue;
-            if (!isValidTools(tool, type)) continue;
-            yield {pos: i.pos, t: i.t + 7, tool: tool, type: type, last: i};
+            if (!isValidTools(tool, terrain)) continue;
+            yield {pos: i.pos, t: i.t + 7, tool: tool, terrain: terrain, last: i};
         }
     };
     var getCost = i => i.t;
